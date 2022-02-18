1695d271e966d8a6c2ae008378e714122627f5e2
turkish-delites <bakkal27@gmail.com>
2022-02-17 20:35:05 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Evan Bakkal on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
